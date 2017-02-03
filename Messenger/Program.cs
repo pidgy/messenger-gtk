967a92d55deee13f3bc07612a7c9c2760023a3d9
NolanHodge <nolanhodge@github.com>
2017-02-03 02:20:02 -0500

    Adding Source

---
+++ Messenger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WebKit;
+using Gtk;
+
+namespace Messenger
+{
+	class MainClass
+	{
+
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			Window window = new Window ("Gtk#Messenger");
+			window.Destroyed += delegate (object sender, EventArgs e) 
+			{
+				Application.Quit ();
+			};
+			window.SetDefaultSize (550,550);
+			WebView webView = new WebView ();
+			webView.Open ("https://messenger.com");
+			webView.SetSizeRequest (500,500);
+			window.Add(webView);
+			window.ShowAll ();
+			Application.Run ();
+		}
+	}
+}
